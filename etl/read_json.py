098ec357502a47997bc7e1ac43648551de9bfa23
arturo.gonzalez <arturo.gonzalez@healthdirect.org.au>
2019-01-29 14:26:48 +1100

    0003 - Improved functions.

--- etl/read_json.py
+++ etl/read_json.py
@@ -1,18 +1,24 @@
 import re
 
-import pyspark
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql import functions as func
+from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import FloatType, StringType, StructType, StructField, MapType, ArrayType, Row, DataType, \
     BooleanType
 from shapely import wkt
-from shapely.geometry import shape, Polygon, mapping
+from shapely.geometry import shape, Polygon, mapping, MultiPolygon
 
 from polygon_utils import multi_polygon_area, merge_multi_polygons, may_intersect, intersection_area, \
-    may_intersect_modified
+    may_intersect_modified, to_shape
 
-
-# conf = (pyspark.SparkConf().setAppName('Urban Forest').setMaster("local[2]"))
+config = SparkConf().setAppName('Urban Forest')
+conf = (config.setMaster('local[*]')
+        .set('spark.executor.memory', '4G')
+        .set('spark.num.executors', '8')
+        # .set('spark.executor.cores', '2')
+        .set('spark.driver.memory', '45G')
+        .set('spark.driver.maxResultSize', '10G')).set('default.parallelism', 2)
 
 
 def calculate_multipolygon_area(multipolygons):
@@ -25,8 +31,16 @@ def calculate_multipolygon_area(multipolygons):
     return multi_polygon_area(merged)
 
 
-sc = pyspark.SparkContext.getOrCreate()
+def calculate_multipolygon_bounds(multipolygons):
+    bounds = []
+    for m in multipolygons:
+        bounds.append(to_shape(m).bounds)
+    return bounds
 
+
+sc = SparkContext(conf=conf)
+# sc = SparkContext.getOrCreate(conf=conf)
+# sc.setLogLevel(newLevel)
 sql_context = SQLContext(sc)
 # read raw data
 df = sql_context.read.json(
@@ -35,42 +49,23 @@ df = df.drop('_corrupt_record').dropna()
 
 # extract nested coordinates data
 df.registerTempTable("raw")
-interested_frame = sql_context.sql("SELECT sa2_name16, geometry.coordinates as coordinates FROM raw")
+interested_frame = sql_context.sql("select sa2_name16 as suburb_name , sa2_main16 as suburb_code, geometry.coordinates as coordinates FROM raw")
 # clean coordinates data
-
 interested_frame.registerTempTable('interested_table')
 user_define_function = func.udf(calculate_multipolygon_area, FloatType())
 gr = sql_context.sql(
-    "SELECT sa2_name16, collect_list(coordinates) AS multipolygon FROM interested_table GROUP BY sa2_name16")
+    "SELECT suburb_name, first(suburb_code) as suburb_code, collect_list(coordinates) AS multipolygon FROM interested_table GROUP BY suburb_name")
 
-# gr = sql_context.sql("SELECT sa2_name16, collect_list(clean_coordinates) AS multipolygon FROM interested_table GROUP BY sa2_name16")
+df1 = gr.withColumn('suburb_area', user_define_function('multipolygon'))
 
-df1 = gr.withColumn('area', user_define_function('multipolygon')).withColumnRenamed('sa2_name16', 'suburb_name')
+multipolygons_bounds_udf = func.udf(calculate_multipolygon_bounds, ArrayType(ArrayType(FloatType())))
+df1 = df1.withColumn('suburb_bound', multipolygons_bounds_udf('multipolygon'))
 
-# def clean_multipolygon(multipolygon):
-#
-#     new_multipolygon = []
-#     for polygon in multipolygon:
-#         new_polygon = []
-#         for loop in polygon:
-#             if len(loop) < 3:
-#                 continue
-#
-#             new_polygon.extend(loop)
-#         if new_polygon:
-#             new_multipolygon.append(new_polygon)
-#         Polygon(new_polygon)
-#
-#     return new_multipolygon
-#
-#
-# interested_frame = interested_frame.withColumn('clean_coordinates',
-#                                                func.udf(clean_multipolygon,
-#                                                         ArrayType(ArrayType(ArrayType(FloatType()))))(interested_frame.coordinates)).drop('coordinates').withColumnRenamed('clean_coordinates', 'coordinates')
+
 
 #########################################forest dataframe
-match = re.compile('(\s[^\s]*)\s')
 
+match = re.compile('(\s[^\s]*)\s')
 
 def convert(value):
     geometry_type, coordinates = value.split(' ', 1)
@@ -90,27 +85,61 @@ df2 = df2.withColumn('polygon_formatted', func.udf(convert, StringType())('polyg
     'polygon_formatted', 'polygon')
 
 
-def join_condition(multipolygons, polygon):
-    polygon_object = wkt.loads(polygon)
+def calculate_forest_bound(polygon):
+    return wkt.loads(polygon).bounds
 
-    return may_intersect_modified(multipolygons, polygon_object)
+
+calculate_forest_bound_udf = func.udf(calculate_forest_bound, ArrayType(FloatType()))
+df2 = df2.withColumn('forest_bound', calculate_forest_bound_udf('polygon'))
+
+
+def join_condition(suburb_bound, forest_bound):
+    def intersect(box_a, box_b):
+        a_min_x, a_min_y, a_max_x, a_max_y = box_a
+        b_min_x, b_min_y, b_max_x, b_max_y = box_b
+        return a_min_y <= b_max_y and \
+               a_max_x >= b_min_x and \
+               a_max_y >= b_min_y and \
+               a_min_x <= b_max_x
+
+    for sub in suburb_bound:
+        if intersect(sub, forest_bound):
+            return True
+
+    return False
 
 
 join_condition_udf = func.udf(join_condition, BooleanType())
-joined = df1.select(func.col("suburb_name"), func.col("multipolygon"), func.col("area").alias('suburb_area')).crossJoin(
-    df2).where(join_condition_udf(df1.multipolygon, df2.polygon))
+df1p1 = df1.repartition(1)
+df2p1 = df2.repartition(10)
+joined = df1p1.crossJoin(df2p1).where(join_condition_udf(df1p1.suburb_bound, df2p1.forest_bound))
+
+joined = joined.drop('suburb_bound').drop('area').drop('forest_bound')
+
 joined.registerTempTable('joined_table')
-grouped_df = sql_context.sql(
-    'select suburb_name, first(multipolygon) as multipolygon, first(suburb_area) as suburb_area, sum(area) as area, collect_list(polygon) as forest_multipolygon from joined_table group by suburb_name')
+sql_context.cacheTable("joined_table")
+a = sql_context.sql('select count(*) from joined_table')
+a.show()
+# result = sql_context.sql(
+#     'select suburb_code, first(multipolygon) as multipolygon, first(suburb_area) as suburb_area, collect_list(polygon) '
+#     'as forest_multipolygon from joined_table group by suburb_code')
+# forest_grouped = sql_context.sql(
+#     'select suburb_code, collect_list(polygon) as forest_multipolygon from joined_table group by suburb_code')
 
+# joined.groupBy('suburb_code').agg({'polygon': 'collect_list'}).show()
 
-def calculate_forest_rate(multipolygons, forest_multipolygon, suburb_area):
-    merged_suburb = merge_multi_polygons(*multipolygons)
-    merge_forest = merge_multi_polygons(*[wkt.loads(m) for m in forest_multipolygon])
-    return round(intersection_area(merged_suburb, merge_forest) / suburb_area, 3)
+# forest_grouped_p = forest_grouped.repartition(1)
+# result = df1p1.join(forest_grouped_p, df1.suburb_name == forest_grouped.suburb_name, how='inner')
+# result.show()
 
-
-result_df = grouped_df.withColumn('per',
-                                  func.udf(calculate_forest_rate, FloatType())('multipolygon', 'forest_multipolygon',
-                                                                               'suburb_area')).orderBy(func.desc("per"))
-result_df.show()
+# def calculate_forest_rate(multipolygons, forest_multipolygon, suburb_area):
+#     raise ValueError('test')
+#     merged_suburb = merge_multi_polygons(*multipolygons)
+#     merge_forest = merge_multi_polygons(*[wkt.loads(m) for m in forest_multipolygon])
+#     return round(intersection_area(merged_suburb, merge_forest) / suburb_area, 3)
+#
+# result = result.withColumn('per', func.udf(calculate_forest_rate, FloatType())('multipolygon', 'forest_multipolygon', 'suburb_area'))
+#
+# result.show(n=2)
+# result = result.orderBy('per', ascending=False)
+# result.show(n=2)
--- polygon_utils.py
+++ polygon_utils.py
@@ -1,4 +1,7 @@
-from shapely.geometry import asShape
+from itertools import chain
+
+from shapely.geometry import asShape, Polygon, MultiPolygon
+from shapely.geometry.base import BaseGeometry
 from shapely.ops import unary_union
 
 
@@ -67,7 +70,9 @@ def to_multi_polygon(multi_polygon_shape):
 def merge_multi_polygons(*multi_polygons):
     mps = []
     for mp in multi_polygons:
-        mps.append(to_shape(mp))
+        if not isinstance(mp, BaseGeometry):
+            mp = to_shape(mp)
+        mps.append(mp)
     union = unary_union(mps)
     if union.geom_type == 'MultiPolygon':
         res = to_multi_polygon(union)
@@ -77,13 +82,30 @@ def merge_multi_polygons(*multi_polygons):
 
 
 def may_intersect(multi_polygon_a, multi_polygon_b):
-    a_min_x, a_min_y, a_max_x, a_max_y = to_shape(multi_polygon_a).bounds
-    b_min_x, b_min_y, b_max_x, b_max_y = to_shape(multi_polygon_b).bounds
+
+    multi_polygon_a = to_shape(multi_polygon_a)
+    multi_polygon_b = to_shape(multi_polygon_b)
+    a_min_x, a_min_y, a_max_x, a_max_y = multi_polygon_a.bounds
+    b_min_x, b_min_y, b_max_x, b_max_y = multi_polygon_b.bounds
 
     return a_min_y <= b_max_y and \
            a_max_x >= b_min_x and \
            a_max_y >= b_min_y and \
            a_min_x <= b_max_x
+
+
+def may_intersect_modified(multi_polygons, multi_polygon_b):
+    b_min_x, b_min_y, b_max_x, b_max_y = multi_polygon_b.bounds
+    for multi_polygon in multi_polygons:
+        for polygon in multi_polygon:
+            a_min_x, a_min_y, a_max_x, a_max_y = Polygon(list(chain(*polygon))).bounds
+            if a_min_y <= b_max_y and \
+               a_max_x >= b_min_x and \
+               a_max_y >= b_min_y and \
+               a_min_x <= b_max_x:
+                return True
+
+    return False
 
 
 def multi_polygon_area(multi_polygon):
